410731768df57cf0c00a034c16aaafbba9f839ec
cliftonbartholomew <cliftonbartholomew@gmail.com>
2023-03-29 11:47:08 +0200

    changed floatterm keybindings

--- nvim/after/plugin/floatterm.lua
+++ nvim/after/plugin/floatterm.lua
@@ -1,5 +1,7 @@
--- terminal mode remap Esc
-vim.keymap.set("t", "<Esc>", "<C-\\><C-n><cmd>FloatermToggle<CR>")
-
--- toggle terminal
-vim.keymap.set("n", "<leader>t", "<cmd>FloatermToggle<CR>")
+vim.keymap.set("n", "<F7>", "<cmd>FloatermNew<CR>")
+vim.keymap.set("t", "<F7>", "<C-\\><C-n><cmd>FloatermNew<CR>")
+vim.keymap.set("t", "<F8>", "<C-\\><C-n><cmd>FloatermPrev<CR>")
+vim.keymap.set("t", "<F9>", "<C-\\><C-n><cmd>FloatermNext<CR>")
+vim.keymap.set("t", "<F10>", "<C-\\><C-n><cmd>FloatermKill<CR><cmd>FloatermToggle<CR>")
+vim.keymap.set("n", "<F12>", "<cmd>FloatermToggle<CR>")
+vim.keymap.set("t", "<F12>", "<C-\\><C-n><cmd>FloatermToggle<CR>")
