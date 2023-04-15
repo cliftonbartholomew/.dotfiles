188a74de883987076ce50f4b9d2aaa4f696876f4
cliftonbartholomew <cliftonbartholomew@gmail.com>
2023-04-15 12:39:55 +0200

    added packersync to run in the command line for setup.sh

--- setup.sh
+++ setup.sh
@@ -36,3 +36,6 @@ sudo apt install virtualenv
 #download packer
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+ 
+#run PackerSync
+nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
